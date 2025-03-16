d01eb4a6ea32a382072bb6e7839671cb7926e5da
jmattaa <mattajonathan1@gmail.com>
2025-03-16 11:00:01 +0100

    use local javy exec

--- buildplugin.sh
+++ buildplugin.sh
@@ -5,4 +5,4 @@ outfile=out.js
 npx tsc --noEmit
 npx esbuild --bundle src/app.ts --tree-shaking=true --format=esm --target=es2020 --outfile=$outfile
 
-javy compile $outfile -o examples/plugin.wasm
+./javy compile $outfile -o examples/plugin.wasm
