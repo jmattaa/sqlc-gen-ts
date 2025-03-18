56765562162c5fa21b91b836556d56015897466c
Jonathan Matta <mattajonathan1@gmail.com>
2025-03-18 17:17:51 +0100

    chore(build): Update build script for plugin gen, use global javy file if exists

--- buildplugin.sh
+++ buildplugin.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+set -x
+
 outfile=out.js
 
 npx tsc --noEmit
 npx esbuild --bundle src/app.ts --tree-shaking=true --format=esm --target=es2020 --outfile=$outfile
 
-./javy build $outfile -o examples/plugin.wasm
+javy_cmd=$(command -v javy || echo "./javy")
+$javy_cmd build $outfile -o ./plugin.wasm
